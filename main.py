53a64dd1a2f67a51e225cd48e0498a99e02f1e34
minpeter <kali2005611@gmail.com>
2022-07-21 08:31:37 +0900

    cors fix

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from scraper import naver_scraper, danawa_scraper
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
